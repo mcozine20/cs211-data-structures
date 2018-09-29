18478d1597e9b8e61b97851a8c1c0373aec9d241
mclean <36020076+mcozine20@users.noreply.github.com>
2018-09-29 20:41:41 +0000

    Added part that didn't copy before

--- hw10-dijkstras/Heap.java
+++ hw10-dijkstras/Heap.java
@@ -27,3 +27,74 @@ public class Heap {
 	}
 	my_heap = temp;
     }
+
+    public void siftUp(int pos) {
+	if(pos != 1 && my_heap[pos].compareTo(my_heap[pos/2]) < 0) {
+	    swap(pos, pos/2);
+	    siftUp(pos/2);
+	}
+    }
+
+    public Node remove() {
+	if(count == 0) return null;
+
+	Node temp = my_heap[1];
+	my_heap[1] = my_heap[count];
+	count--;
+	siftDown(1);
+	return temp;
+    }
+
+    public void siftDown(int pos) {
+	if(2 * pos > count) return;
+
+	if(2 * pos == count) {
+	    if(my_heap[2 * pos].compareTo(my_heap[pos]) < 0) {
+		swap(pos, 2 * pos);
+	    }
+	}
+	else {
+	    int smaller;
+	    if(my_heap[2 * pos].compareTo(my_heap[2 * pos + 1]) <= 0) smaller = 2 * pos;
+	    else smaller = 2 * pos + 1;
+	    
+	    if(my_heap[pos].compareTo(my_heap[smaller]) > 0) {
+		swap(pos, smaller);
+		siftDown(smaller);
+	    }
+	}
+    }
+
+    public void swap(int i, int j) {
+	Node temp = my_heap[i];
+	my_heap[i] = my_heap[j];
+	my_heap[j] = temp;
+    }
+
+    public void print() {
+	for(int i = 1; i <= count; i++) {
+	    System.out.println(my_heap[i]);
+	}
+    }
+
+    public void decreaseKey(Node value, int newKey) {
+	if (newKey < value.dist) {
+	    value.dist = newKey;
+	    int pos = 0;
+
+	    for (int i = 1; i <= my_heap.length; i++) {
+		if (my_heap[i] == value) {
+		    pos = i;
+		    break;
+		}
+	    }
+
+	    siftUp(pos);
+	}
+    }
+
+    public Heap() {
+	my_heap = new Node[2];
+    }
+
+} 
